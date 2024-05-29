92299324c58847520af4bffe21c3819aa0352187
qomsad <149834513+qomsad@users.noreply.github.com>
2024-05-30 02:41:00 +0500

    Auth Extensions

---
+++ Auth/AuthExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace AspBoot.Auth;
+
+public static class AuthExtensions
+{
+    public static string? GetPrincipal(this ClaimsPrincipal principal, string type)
+    {
+        return principal.Claims.FirstOrDefault(claim => claim.Type == type)?.Value;
+    }
+}

87df5a384aa40baeef7045b6d6d4b8b86ac1244f
qomsad <149834513+qomsad@users.noreply.github.com>
2024-05-30 02:42:07 +0500

    Validator pipe

---
+++ Validation/ValidationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace AspBoot.Validation;
+
+public class ValidationResult<TResult>
+    where TResult : class
+{
+    public IEnumerable<ValidationProblem>? Problems { get; init; }
+    public TResult? Request { get; init; }
+}
+
+public class ValidationProblem
+{
+    public required string Property { get; set; }
+    public required string Code { get; set; }
+    public required string Description { get; set; }
+}
---
+++ Validation/Validator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace AspBoot.Validation;
+
+public static class Validator
+{
+    public static ValidationResult<TRequest> GetValidationProblems<TRequest>(
+        this IValidator<TRequest> validator,
+        TRequest request) where TRequest : class
+    {
+        var result = validator.Validate(request);
+
+        var problems = result.Errors.Select(error => new ValidationProblem
+        {
+            Property = error.PropertyName,
+            Code = error.ErrorCode,
+            Description = error.ErrorMessage
+        });
+
+        return new ValidationResult<TRequest>
+        {
+            Problems = result.IsValid ? null : problems, Request = result.IsValid ? request : null
+        };
+    }
+}

3bd5ad72082509319b9e190b0823e21bc859eb35
qomsad <149834513+qomsad@users.noreply.github.com>
2024-05-31 21:31:36 +0500

    date utils

---
+++ Utils/DateUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace AspBoot.Utils;
+
+public static class DateUtils
+{
+    public static bool IsLast(this DateTime? time, int minutes)
+    {
+        if (time == null)
+        {
+            return false;
+        }
+        var t1 = (DateTime) time;
+        return DateTime.Now.ToUniversalTime().Subtract(t1).Minutes <= minutes;
+    }
+}
